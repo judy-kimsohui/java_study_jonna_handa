b6c5fa146e6d2b9bc63d091067e31c47b156055a
김소희 <83997634+judy-kimsohui@users.noreply.github.com>
2026-02-02 13:35:50 +0900

    Update Password_1128.java

--- Password_1128.java
+++ Password_1128.java
@@ -1,57 +1,37 @@
 package algorithm;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Password_1128 {
-	
-	public static void main(String[] args) throws Exception {
-		//	 I(삽입) x, y, s : 앞에서부터 x의 위치 바로 다음에 y개의 숫자를 삽입한다.
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for (int t = 1; t <= 10; t++) {
-			
-		    int N = Integer.parseInt(br.readLine());
+public class Password_1129_mod {
+    public static void main(String[] args) throws Exception {
 
-		    String input = br.readLine();
-		    String[] rawPassword = input.split("\\s+");
-		    
-		    LinkedList<Integer> list = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		    for (String s : rawPassword) {
-		        list.add(Integer.parseInt(s));
-		    }
+        for (int t = 1; t <= 10; t++) {
 
-		    int M = Integer.parseInt(br.readLine());
+            br.readLine(); // N 
 
-		    input = br.readLine();
-		    String[] command = input.split("\\s*\\|\\s*");
+            LinkedList<Integer> list = new LinkedList<>();
+            for (String s : br.readLine().split(" "))
+                list.add(Integer.parseInt(s));
 
-//		    System.out.println(Arrays.toString(command));
-		    
-		    StringTokenizer st = new StringTokenizer(input);
+            br.readLine(); // M 
 
-		    while (st.hasMoreTokens()) {
-		        String cmd = st.nextToken(); // "I"
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                st.nextToken(); // "I"
+                int idx = Integer.parseInt(st.nextToken());
+                int cnt = Integer.parseInt(st.nextToken());
 
-		        if (cmd.equals("I")) {
-		            int idx = Integer.parseInt(st.nextToken());
-		            int cnt = Integer.parseInt(st.nextToken());
+                for (int i = 0; i < cnt; i++)
+                    list.add(idx + i, Integer.parseInt(st.nextToken()));
+            }
 
-		            for (int i = 0; i < cnt; i++) {
-		                list.add(idx + i, Integer.parseInt(st.nextToken()));
-		            }
-		        }
-		    }
-		    
-		    System.out.print("#" + t + " ");
-
-		    // 확인용
-		    for (int i = 0; i < 10; i++) {
-		        System.out.print(list.get(i) + " ");
-		    }
-		    System.out.println();
-		
-		}		
-	}
+            System.out.print("#" + t + " ");
+            for (int i = 0; i < 10; i++)
+                System.out.print(list.get(i) + " ");
+            System.out.println();
+        }
+    }
 }
